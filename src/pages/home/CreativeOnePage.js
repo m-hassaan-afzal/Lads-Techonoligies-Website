fb44968e0f8a8728a936c526a79a4928034e6943
Your Name <you@example.com>
2022-01-20 15:23:06 +0500

    content change

--- src/pages/home/CreativeOnePage.js
+++ src/pages/home/CreativeOnePage.js
@@ -1,4 +1,4 @@
-import React, { useState,useRef } from "react";
+import React, { useState, useRef } from "react";
 import Loader from "./../../components/Loader/Loader";
 import dataCreative from "../../data/Slider/creative-one-page-data.json";
 import serviceOneImg from "../../assets/images/onepage-bg-left.jpg";
@@ -22,7 +22,7 @@ const CreativeOnePage = () => {
   const service = useRef();
   const team = useRef();
   const Testimonial = useRef();
-  const contact = useRef(); 
+  const contact = useRef();
 
 
   const scrollToSection = (e, content) => {
@@ -34,22 +34,22 @@ const CreativeOnePage = () => {
       case "about":
         about.current.scrollIntoView({ behavior: "smooth" });
         break;
-        case "feature":
+      case "feature":
         feature.current.scrollIntoView({ behavior: "smooth" });
         break;
       case "service":
         service.current.scrollIntoView({ behavior: "smooth" });
         break;
-      
+
       case "team":
         team.current.scrollIntoView({ behavior: "smooth" });
         break;
-     
+
       case "contact":
         contact.current.scrollIntoView({ behavior: "smooth" });
         break;
       default:
-        
+
     }
   };
 
@@ -57,9 +57,9 @@ const CreativeOnePage = () => {
     <Loader>
       <HeaderTwo scrollToSection={scrollToSection} />
       <HeroSliderSection data={dataCreative} ref={home} />
-    <div ref={about}
-   >
-     {/* <WhoWeAreOne
+      <div ref={about}
+      >
+        {/* <WhoWeAreOne
       title="We’r Full Fledged Digital Agency Creating Awesome Things"
       tagline="We Love What We Do"
       verticalWord="Design"
@@ -67,53 +67,47 @@ const CreativeOnePage = () => {
     >
       We develop innovative and creative products and services that provide total communication and information solutions. Our emphasis is on offering a high degree of product user friendliness through a positive, creative and focused company staff.
     </WhoWeAreOne> */}
-    <AboutAppStyle ref={feature} />
-    <CounterOne />
-       
-      </div> 
+        <AboutAppStyle ref={feature} />
+        <CounterOne />
+
+      </div>
       <div ref={service}>
         <OurServices
           title="What We Do"
           tagline="We develop big ideas that sell."
-          serviceImg={serviceOneImg}   
+          serviceImg={serviceOneImg}
         />
-       
-       </div>
 
-       <div ref={team}>
-      <OurTeam title="MEET OUR TEAM" tagline="WE ARE STRONGER" bg="dark" />
-      
+      </div>
+
+      <div ref={team}>
+        <OurTeam title="MEET OUR TEAM" tagline="WE ARE STRONGER" bg="dark" />
+
       </div>
 
       <div ref={Testimonial}>
-        
+
       </div>
-     
+
       <TestimonialsOne title="Testimonials" tagline="Happy clients" />
 
       {/* <TestimonialItem /> */}
-      
+
+      <div ref={contact}>
+
+        <CTAOne
+          tagline="careers"
+          title="Let's write your story, together"
+          textButton="Contact us"
+          linkButton="contactus"
+        >
+          We believe in “learn one, do one, teach one”. Each new member of Our team is assigned a mentor for their professional, personal and career development. Lets Work Together send us your resume:
+
+        </CTAOne>
+        <FooterOne />
+      </div>
 
 
-      
-     
-      
-
-        <div ref={contact}>
-        
-      <CTAOne
-      tagline="careers"
-      title="Let's write your story, together"
-      textButton="Contact us"
-      linkButton="contactus"
-    >
-     We believe in “learn one, do one, teach one”. Each new member of Our team is assigned a mentor for their professional, personal and career development. Lets Work Together send us your resume:
-      
-    </CTAOne>
-    <FooterOne />
-    </div>
-    
-   
     </Loader>
   );
 };
